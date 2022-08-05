c103d2b244b5de0379d6fe3cff733c734b601995
cmderobertis <cmderobertis@gmail.com>
2022-08-04 20:34:21 -0700

    fixed info on checkout.html to reflect form data

--- server.py
+++ server.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+import datetime
+app = Flask(__name__)
 
-@app.route('/')         
+
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
-def checkout():
-    print(request.form)
-    return render_template("checkout.html")
 
-@app.route('/fruits')         
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    data = request.form
+    num_items = int(data['strawberry']) + \
+        int(data['raspberry']) + int(data['apple'])
+    now = datetime.datetime.now()
+    day = now.strftime("%x")
+    time = now.strftime("%X")
+    print(f"Charging {data['first_name']} for {num_items} fruits")
+    return render_template("checkout.html", data=data, num_items=num_items, day=day, time=time)
+
+
+@app.route('/fruits')
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+
+if __name__ == "__main__":
+    app.run(debug=True)
